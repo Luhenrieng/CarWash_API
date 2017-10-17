a245f4648cf92efb588e3bf8424b93702040b3ff
luhenri22 <luizhenri222@gmail.com>
2017-10-16 22:54:28 -0200

    First commit

---
+++ CarWash.BasicApplication/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,90 @@
+
+namespace BasicDDD.BasicApplication.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using Domain.Interfaces.Service;
+    using Domain.Services;
+    using Application;
+    using Application.Interface;
+    using Domain.Interfaces;
+    using Infra.Data.Repositories;
+    using System.Web.Http;
+    using System.Collections.Generic;
+    using System.Web.Http.Dependencies;
+    using Ninject.Syntax;
+    using System.Diagnostics.Contracts;
+    using Ninject.Modules;
+
+    
+
+    public class NinjectRegistrations : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IClientService>().To<ClientService>();
+            Bind<IClientAppService>().To<ClientAppService>();
+            Bind<IClientRepository>().To<ClientRepository>();
+        }
+    }
+
+
+    public class NinjectDependencyScope : IDependencyScope
+    {
+        private IResolutionRoot resolver;
+
+        internal NinjectDependencyScope(IResolutionRoot resolver)
+        {
+            Contract.Assert(resolver != null);
+
+            this.resolver = resolver;
+        }
+
+        public void Dispose()
+        {
+            IDisposable disposable = resolver as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+
+            resolver = null;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed");
+
+            return resolver.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed");
+
+            return resolver.GetAll(serviceType);
+        }
+    }
+
+
+    public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver, System.Web.Mvc.IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+            : base(kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(kernel.BeginBlock());
+        }
+    }
+}
---
+++ CarWash.BasicApplication/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BasicDDD.BasicApplication.Models;
+using BasicDDD.Domain.Entities;
+using System.Collections.Generic;
+
+namespace BasicDDD.BasicApplication.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Client, ClientViewModel>();
+        }
+    }
+}
